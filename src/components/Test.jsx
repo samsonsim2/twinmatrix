3ff9b0cb66ee3721b6e1a9830eb67f387f713a23
Samson Sim <samsonsim2@gmail.com>
2024-07-24 14:48:59 +0800

    fixed glitch

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -8,17 +8,17 @@ export default function Test(props) {
   const facadeMaterial= <meshStandardMaterial color="red" side={DoubleSide}/>
   return (
     <group {...props} dispose={null} scale={0.008}>
-      {/* <mesh
+      <mesh
         castShadow
         receiveShadow
         geometry={nodes.Facade.geometry}
-        material={facadeMaterial}
-      >  <meshStandardMaterial color={"#f9f9f9"}  opacity={0.5} transparent={true}/></mesh> */}
+       
+      >  <meshStandardMaterial color={"#f9f9f9"}  opacity={0.5} transparent={true}/></mesh>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Beams_and_Column.geometry}
-        material={facadeMaterial}
+         
         position={[18.998, 16.229, 503.332]}
        > <meshStandardMaterial color={"#1F51FF"} side={DoubleSide}/></mesh>
     </group>
